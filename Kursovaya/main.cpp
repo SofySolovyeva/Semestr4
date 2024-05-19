6b19143752b8bc19c78051bae7186c753d81e317
sofya <sofysolovyeva@gmail.com>
2024-05-19 22:30:26 +0300

    арена

--- Kursovaya/main.cpp
+++ Kursovaya/main.cpp
@@ -1,11 +1,9 @@
 #include "arena.hpp"
-#include "character.hpp"
-#include "point2d.hpp"
-#include "predator.hpp"
-#include "prey.hpp"
 
 int main() {
-    
+    srand(static_cast<unsigned int>(time(0)));
     setlocale(LC_ALL, "Russian");
+    Arena arena;
+    arena.play();
     return 0;
 }
